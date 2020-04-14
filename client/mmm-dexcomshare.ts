ecf4a1db60e0db6babf2b258317f7480b86bac87
Chris Speers <chris.speers@outlook.com>
2020-04-13 20:49:40 -0500

    initial commit

---
+++ client/mmm-dexcomshare.ts
@@ -0,0 +1,255 @@
+'use strict'
+
+/** wrapper for the Magic Mirror logger */
+let ModuleLogger:ILogger={
+    info:(m:string):void => Log.info(`[${ModuleDetails.name}] ${m}`),
+    warn:(m:string):void => Log.warn(`[${ModuleDetails.name}] ${m}`),
+    error:(m:string):void => Log.error(`[${ModuleDetails.name}] ${m}`)
+};
+
+interface IDexcomModuleProperties extends IModuleProperties {
+    version:string
+    defaults:IDexcomModuleConfig
+    canvas:HTMLCanvasElement
+    /** subclass of the notification received event */
+    notificationReceived:ModuleNotificationEvent
+    socketNotificationReceived:ISocketNotificationEvent<DexcomModuleNotificationType,IDexcomGlucoseEntryMessage>
+    currentBG:IDexcomShareGlucoseEntry
+    previousBG:IDexcomShareGlucoseEntry
+}
+
+const defaultChartOptions: Chart.ChartOptions = {
+    responsive: true,
+    title: {
+        display: false,
+        text: 'Blood Sugar Values (mg/dl)'
+    },
+    scales: {
+        xAxes: [{
+            type: 'time',
+            display: true,
+            distribution: 'series',
+            time: {
+                parser: 'YYYY-MM-DD HH:mm:ss',
+                unit: 'minute',
+                unitStepSize: 30
+            },
+            ticks: {
+                source: 'auto',
+                autoSkip: true
+            },
+            scaleLabel: {
+                display: true,
+                labelString: "Date"
+            },
+            gridLines: {
+                display: true,
+                offsetGridLines: true,
+                color: "rgba(0, 0, 0, 0)",
+            }
+        }],
+        yAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: 'mg/dL'
+            },
+            ticks: {
+                beginAtZero:true,
+                min:30,
+                max:400
+            },
+            gridLines: {
+                display: true,
+                color: "rgba(0, 0, 0, 0)"
+            }
+        }]
+    },
+    legend: {
+        display: false,
+        position: "bottom"
+    }
+}
+
+let dexcomModule:IDexcomModuleProperties = {
+    name:ModuleDetails.name,
+    version:ModuleDetails.version,
+    identifier:undefined,
+    data:undefined,
+    config:undefined,
+    canvas:undefined,
+    currentBG:undefined,
+    previousBG:undefined,
+    defaults:{
+        userName:undefined,
+        password:undefined,
+        entryLength:1440,
+        server:"share1.dexcom.com",
+        refreshInterval:300,
+        applicationId:"d89443d2-327c-4a6f-89e5-496bbb0317db",
+        chartOptions:defaultChartOptions,
+        chartType:'line',
+        highRange:185,
+        lowRange:70,
+        fill:false,
+        showX:true,
+        showY:true
+    },
+    hidden:false,
+    renderChart(bgValues:Array<IDexcomShareGlucoseEntry>) {
+        
+        //These should be ordered descending by time
+        this.currentBG=bgValues[0]
+        this.previousBG=bgValues[1]
+
+        let bsgs=bgValues.map(a=>a.Value);
+        let maxInSeries=Math.max.apply(bsgs.slice(0,1),bsgs.slice(1))
+        let minInSeries=Math.min.apply(bsgs.slice(0,1),bsgs.slice(1))
+
+        let delta=this.currentBG.Value - this.previousBG.Value
+
+        ModuleLogger.info(`BG Values ${this.currentBG.DirectionAsUnicode} Current:${this.currentBG.Value} Previous:${this.previousBG.Value}
+            Min:${minInSeries} Max:${maxInSeries}
+            Direction:${this.currentBG.Direction}Delta:${delta}`)
+
+        let lowBgVals=bgValues.filter(e => +e.Value <= this.config.lowRange).map(a=>{return {x:a.WT,y:a.Value}})
+        let inRangeVals=bgValues.filter(e => +e.Value >= this.config.lowRange && +e.Value <=this.config.highRange).map(a=>{return {x:a.WT,y:a.Value}})
+        let highVals=bgValues.filter(e => +e.Value >= this.config.highRange).map(a=>{return {x:a.WT,y:a.Value}})
+
+        let bgValSpan=document.querySelector('#current-bsg-value')
+        let deltaSign=delta>=0 ? '+' : '-'
+        
+        let fontColor= '#5AB05A'
+        if(this.currentBG >= this.config.highRange){
+            fontColor='#E5E500'
+        }
+        else if(this.currentBG < this.config.lowRange){
+            fontColor='#E50000'
+        }
+
+        let infoDiv = document.createElement("div");
+        infoDiv.setAttribute('style',"float: left;clear: none;");
+        
+        let bs = document.createElement("div");
+        bs.setAttribute('style',"display: table;");
+        bs.innerHTML =`
+            <span class="bright medium light" style="display: table-cell;vertical-align:top;color:${fontColor}">${this.currentBG.Value}</span>
+            <span class="bright medium light" style="display: table-cell;vertical-align:top;">${this.currentBG.DirectionAsUnicode}</span>
+        `;
+        
+        infoDiv.appendChild(bs);
+        
+        let deltaElem = document.createElement("div");
+        deltaElem.innerHTML=`<span class="bright small light" style="display: table-cell;vertical-align:top;">${deltaSign} ${delta} mg/dL</span>`
+        infoDiv.appendChild(deltaElem)
+
+        bgValSpan.innerHTML=infoDiv.outerHTML
+        //we'll use the simple setting from the config if a full chartjs one wasn't given
+        if(this.config.chartOptions.title.text==='Blood Sugar Values (mg/dl)' && !this.config.chartOptions.title.display){
+            this.config.chartOptions.scales.xAxes[0].display=this.config.showX
+            this.config.chartOptions.scales.yAxes[0].display=this.config.showY
+        }
+
+        let chartData:Chart.ChartData = {
+            datasets: [
+                {
+                    label: `Low <= ${this.config.lowRange} mg/dL`,
+                    data: lowBgVals,
+                    borderColor: 'red',
+                    fill:this.config.fill,
+                    pointRadius: 2
+                },
+                {
+                    label: "In Range",
+                    data: inRangeVals,
+                    borderColor: 'limegreen',
+                    fill:this.config.fill,
+                    pointRadius: 2
+                },
+                {
+                    label: `High >= ${this.config.highRange} mg/dL`,
+                    data: highVals,
+                    borderColor: 'yellow',
+                    fill:this.config.fill,
+                    pointRadius: 2
+                }
+            ]
+        }
+        let chartConfig:Chart.ChartConfiguration={
+            type: this.config.chartType,
+            data: chartData,
+            options: this.config.chartOptions
+        }
+        let glucoseChart=new Chart(this.canvas,chartConfig)
+    },    
+    getScripts(){
+        return [
+            this.file("./node_modules/chart.js/dist/Chart.bundle.js")
+        ]
+    },
+    getStyles(){
+        return [
+            "mmm-dexcomshare.css"
+        ]
+    },
+    getDom() {
+        let wrapper=document.createElement('div')
+        wrapper.classList.add('magicmirror-dexcomshare')
+
+        let moduleContent=document.createElement('div')
+        moduleContent.id='magicmirror-dexcomshare-content'
+
+        let bgDetail=document.createElement('div')
+        let bgNumberSpan=document.createElement('div')
+        bgNumberSpan.id='current-bsg-value'
+        
+        bgDetail.appendChild(bgNumberSpan)
+
+        moduleContent.appendChild(bgDetail)
+
+        let glucoseCanvas=document.createElement('canvas')
+        glucoseCanvas.id='glucoseChart'
+        moduleContent.appendChild(glucoseCanvas)
+        this.canvas=glucoseCanvas
+
+        wrapper.appendChild(moduleContent)
+
+        return wrapper;
+    },
+    start(){
+        ModuleLogger.info(`Starting up...`);
+        this.sendSocketNotification('START_FETCHING',this.config)
+    },
+    stop(){
+        ModuleLogger.info(`Starting up...`);
+    },
+    suspend(){
+        ModuleLogger.info(`Module Suspended...`)
+    },
+    resume(){
+        ModuleLogger.info(`Module Resumed...`)
+    },
+    notificationReceived(message:ModuleNotificationType,payload:any,sender?:IModuleInstance){
+        switch(message) {
+            case 'MODULE_DOM_CREATED':
+                break;
+        }
+    },
+    socketNotificationReceived(message:DexcomModuleNotificationType,payload:IDexcomGlucoseEntryMessage){
+        ModuleLogger.info(`received socket notification ${message}`);
+        switch (message) {
+            case 'BLOODSUGAR_VALUES':
+                if(payload){
+                    ModuleLogger.info(`Received at ${payload.received} entries ${payload.entries.length}`);
+                    this.renderChart(payload.entries);
+                }
+                break;
+            case 'AUTH_ERROR':
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+Module.register(ModuleDetails.name,dexcomModule)
---
+++ server/node_helper.ts
@@ -0,0 +1,229 @@
+
+'use strict'
+
+import * as NodeHelper from "node_helper";
+import * as os from "os"
+import request, { RequestCallback } from 'request';
+import moment from 'moment'
+
+/** Log wrapper */
+let DexcomHelperLogger:ILogger = {
+    info(message: string) {console.info(`[${ModuleDetails.name}]${message}`)},
+    warn(message: string) {console.warn(`[${ModuleDetails.name}]${message}`)},
+    error(message: string) {console.error(`[${ModuleDetails.name}]${message}`)}
+};
+
+class GlucoseFetcher {
+    config:IDexcomModuleConfig
+    sessionId:string
+    maxCount:number
+    onGlucoseReceived:GlucoseEntriesReceivedCallback
+    stopRequested:boolean
+
+    //#region Glucose Helpers
+
+    private trendToUnicode(trend:DexcomTrend):string {
+        switch (trend) {        
+            case DexcomTrend.DoubleUp:
+                return "⇈";
+            case DexcomTrend.SingleUp:
+                return "↑";
+            case DexcomTrend.FortyFiveUp:
+                return "↗";
+            case DexcomTrend.Flat:
+                return "→";
+            case DexcomTrend.FortyFiveDown:
+                return "↘";
+            case DexcomTrend.SingleDown:
+                return "↓";
+            case DexcomTrend.DoubleDown:
+                return "⇊";
+            case DexcomTrend.OutOfRange:
+                return "⇕";
+            case DexcomTrend.None:
+                return "⇼";            
+            default:
+                return "-";
+        }
+    }
+
+    private trendToDirection(trend:DexcomTrend):string{
+        switch(trend){
+            case DexcomTrend.DoubleDown:
+                return 'DoubleDown'
+            case DexcomTrend.DoubleUp:
+                return 'DoubleUp'
+            case DexcomTrend.Flat:
+                return 'Flat'
+            case DexcomTrend.FortyFiveDown:
+                return 'FortyFiveDown'
+            case DexcomTrend.FortyFiveUp:
+                return 'FortyFiveUp'
+            case DexcomTrend.NotComputable:
+                return 'NotComputable'
+            case DexcomTrend.OutOfRange:
+                return 'OutOfRange'
+            case DexcomTrend.SingleDown:
+                return 'SingleDown'
+            case DexcomTrend.SingleUp:
+                return 'SingleUp'
+            case DexcomTrend.None:
+            default:
+                return 'None'
+        }
+    }
+
+    private getSessionId(then:RequestCallback){
+        let reqOpts: request.Options = {
+            uri: `https://${this.config.server}/ShareWebServices/Services/General/LoginPublisherAccountByName`,
+            json: true,
+            method: 'POST',
+            rejectUnauthorized: false,
+            body:{
+                password:this.config.password,
+                applicationId:this.config.applicationId,
+                accountName:this.config.userName
+            },
+            headers: {
+                'User-Agent': "Dexcom Share/3.0.2.11 CFNetwork/711.2.23 Darwin/14.0.0",
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+        request(reqOpts,then);
+    }
+
+    private fetchGlucose(){
+        //If we've got a session id go fetch the entries
+        if (this.sessionId) {
+            let glucoseQuery=`sessionID=${this.sessionId}&minutes=${this.config.entryLength}&maxCount=${this.maxCount}`
+            let glucoseUri=`https://${this.config.server}/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues?${glucoseQuery}`
+            DexcomHelperLogger.info(`Retrieving Glucose readings from ${glucoseUri}`)            
+            let fetchOpts:request.Options={
+                uri:glucoseUri,
+                json:true,
+                method:'POST',
+                body:"",
+                headers: {
+                    'User-Agent': "Dexcom Share/3.0.2.11 CFNetwork/711.2.23 Darwin/14.0.0",
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                rejectUnauthorized:false
+            }
+            let then:RequestCallback=(error:any,response:any,entries:Array<IDexcomShareGlucose<string>>)=>{
+                if(response && response.statusCode<400){
+                    let glucose = entries.map((e)=>{
+                        return {
+                            DT: moment(e.DT).local().toDate(),
+                            ST: moment(e.ST).toDate(),
+                            WT: moment(e.WT).toDate(),
+                            Trend: e.Trend,
+                            Value: e.Value,
+                            Direction: this.trendToDirection(e.Trend),
+                            DirectionAsUnicode:this.trendToUnicode(e.Trend)
+                        }
+                    })
+                    //we should have the entries now
+                    this.onGlucoseReceived(glucose)
+                    DexcomHelperLogger.info(`Success! Will fetch again in ${this.config.refreshInterval} secs.`)
+                    if(!this.stopRequested) {
+                        setTimeout(()=>{this.fetchGlucose()},this.config.refreshInterval * 1000)
+                    }
+                }
+                else{
+                    //we need to go re-authorize and we'll come back
+                    DexcomHelperLogger.info(`The session id is invalid. Refreshing...`)
+                    this.sessionId=null
+                    if(!this.stopRequested){
+                        this.authorize()
+                    }
+                }
+            }
+            if(!this.stopRequested){
+                request(fetchOpts,then)
+            }
+        }
+        //go get the session id
+        else {
+            DexcomHelperLogger.info(`No session id is present. Authorizing...`)
+            if(!this.stopRequested){
+                this.authorize()
+            }
+        }
+    }
+
+    private authorize(){
+        //authenticate
+        this.getSessionId((err:any,res:any,body:string)=>{
+            if (!err && body && res && res.statusCode == 200) {
+                if(body==='00000000-0000-0000-0000-000000000000'){
+                    throw 'Received an empty session id. Please check your Dexcom Credentials'
+                }
+                this.sessionId=body;
+                DexcomHelperLogger.info(`Retrieved new Dexcom Share2 session:${this.sessionId}`)
+                this.fetchGlucose()
+            }
+            else {
+                throw 'Please check your Dexcom Credentials'
+            }
+        })
+    }
+    //#endregion
+
+    start() {
+        this.fetchGlucose()
+    }
+    stop() {
+        this.stopRequested=true
+    }
+
+    constructor(cfg:IDexcomModuleConfig,callback:GlucoseEntriesReceivedCallback){
+        //start the fetch loop
+        this.config=cfg
+        //since it grabs it every 5 minutes
+        this.maxCount= Math.floor(cfg.entryLength/5)
+        this.onGlucoseReceived=callback
+    }
+}
+
+interface IDexcomNodeHelperConfig extends NodeHelper.IHelperConfig {
+    fetcher:GlucoseFetcher,
+    config:IDexcomModuleConfig
+}
+
+let helperConfig:IDexcomNodeHelperConfig = {
+    fetcher:undefined,
+    config:undefined,
+    //Create Fetcher
+    createFetcher(){
+        this.fetcher=new GlucoseFetcher(this.config,(bsg)=>this.broadcastResults(bsg))
+        this.fetcher.start()
+    },
+    socketNotificationReceived(notification:DexcomModuleNotificationType,payload?:DexcomModuleNotificationPayload){
+        if(payload)this.config=payload;
+        switch (notification) {
+            case "START_FETCHING":
+                this.createFetcher()
+                break;      
+            default:
+                break;
+        }
+    },    
+    //Broadcast results
+    broadcastResults(bsgValues:Array<IDexcomShareGlucoseEntry>){
+        DexcomHelperLogger.info(`Received ${bsgValues.length} entries`)
+        this.sendSocketNotification('BLOODSUGAR_VALUES',{received:new Date(),entries:bsgValues})
+    },
+    start(){
+        DexcomHelperLogger.info(`Starting Module Helper version : ${ModuleDetails.version} - ${os.platform()}:${os.arch()}`);  
+    },
+    stop(){
+        DexcomHelperLogger.info(`Stopping Module Helper...`);
+        if(this.fetcher){
+            this.fetcher.stop()
+        }
+    }
+}
+
+module.exports = NodeHelper.create(helperConfig);
